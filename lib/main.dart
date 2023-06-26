dfd28868a4e5cdf4a338a0be70ad210cb1a11548
niltoncosta <niltonb2@gmail.com>
2023-06-26 15:45:41 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_aula/responsividade_media_query.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Flutter Web",
+    debugShowCheckedModeBanner: false,
+    home: ResponsividadeMediaQuery(),
+  ));
+}
+
